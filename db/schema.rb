f8c7db04703d86c4a9d49f6fd8419001e2a4b2ad
vismarach <christian.vismara@nttdata.com>
2016-01-10 23:52:24 +0100

    User id added to Task

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110155437) do
+ActiveRecord::Schema.define(version: 20160110220019) do
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160110155437) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
